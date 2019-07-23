c7894ebfe7fc57a9b9a32e46f6153cd4a94ef03f
Yingzheng Ma <yma39@ford.com>
2019-07-23 14:41:03 -0700

    extract duplicate jsx to new component

--- components/src/index.js
+++ components/src/index.js
@@ -1,52 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Marko
-          </a>
-          <div className="metadata">
-            <span className="date"> Today at 4:00pm</span>
-          </div>
-          <div className="text">Nice Blog Post</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Marko
-          </a>
-          <div className="metadata">
-            <span className="date"> Today at 4:00pm</span>
-          </div>
-          <div className="text">Nice Blog Post</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Marko
-          </a>
-          <div className="metadata">
-            <span className="date"> Today at 4:00pm</span>
-          </div>
-          <div className="text">Nice Blog Post</div>
-        </div>
-      </div>
+      <CommentDetail authorName="Marko" commentContent="ni hao"/>
+      <CommentDetail authorName="Habibi" commentContent="Hala"/>
+      <CommentDetail authorName="Irteza" commentContent="Eyyyyyy"/>
     </div>
   );
 };

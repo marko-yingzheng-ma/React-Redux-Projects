8de86036410f9a78bcff74aac7920907e0560bf6
Yingzheng Ma <yma39@ford.com>
2019-07-23 15:59:16 -0700

    reused approval card component with vairous custom children

--- components/src/index.js
+++ components/src/index.js
@@ -8,10 +8,32 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
         <CommentDetail 
           author="Marko" 
           timeAgo="Today at 4:00pm" 
           content="ni hao"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Habibi" 
+          timeAgo="Today at 2:00pm" 
+          content="Hala"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Irteza" 
+          timeAgo="Today at 10:00pm" 
+          content="wowowowow"
           avatar={faker.image.avatar()}
         />
       </ApprovalCard>

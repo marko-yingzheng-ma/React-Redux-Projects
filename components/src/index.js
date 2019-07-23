9a3161a794b803124492a9608eb800d43ef617b2
Yingzheng Ma <yma39@ford.com>
2019-07-23 15:09:08 -0700

    implemented Approval Card with custom children

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Marko" 
-        timeAgo="Today at 4:00pm" 
-        content="ni hao"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Habibi" 
-        timeAgo="Today at 1:00pm" 
-        content="Hala"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Irteza" 
-        timeAgo="Today at 3:00pm" 
-        content="Eyyyyyy"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Marko" 
+          timeAgo="Today at 4:00pm" 
+          content="ni hao"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

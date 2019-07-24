c75cd2e0ab8d7dca81e4707d7abbecba9d20731b
Yingzheng Ma <yma39@ford.com>
2019-07-24 09:48:19 -0700

    determine user location

---
+++ seasons/src/SeasonalDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonalDisplay = () => {
+  return (
+
+  );
+};
+
+export default SeasonalDisplay;
---
+++ seasons/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+  },(error) => {
+    console.log(error);
+  })
+  return (
+    <div>Hi HIIIIIIII</div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));

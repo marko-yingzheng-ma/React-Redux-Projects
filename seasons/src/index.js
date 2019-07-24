9f868ecf252ed8fddbad78b87d541a1efd3aabea
Yingzheng Ma <yma39@ford.com>
2019-07-24 16:11:56 -0700

    added component lifecycle methods and update state initialization to not be inside of constructor (if babel setup has support for classfields); Babel will transpile it into constructor autonatically behind the scene

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
-  }
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
+    console.log('Component mounted');
     window.navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
       this.setState({ lat: position.coords.latitude });
     },(error) => {
       console.log(error);
-      this.setState({ errorMessage: error.message })
+      this.setState({ errorMessage: error.message });
     })
+  }
+
+  componentDidUpdate() {
+    console.log('Component updated');
   }
 
   render() {

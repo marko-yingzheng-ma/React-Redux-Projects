2c44ba1e62e3a6b3a59b158617c6c8c52d5c41fe
Yingzheng Ma <yma39@ford.com>
2019-07-25 13:41:36 -0700

    initialize pics project with a new searchbar

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image search</label>
+            <input 
+              type="text" 
+              onChange={(event) => console.log(event.target.value)} 
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+};
+
+export default SearchBar;
